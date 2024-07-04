bd75275ac9f171ded86e3bec78defbface119a4a
Vaccummer <2316374406@qq.com>
2024-07-04 15:34:48 +0800

    https://github.com/Vaccummer/am_store.git

--- update.py
+++ update.py
@@ -9,9 +9,7 @@ def update():
     os.rename(pro_dir, pro_dir+"_bak")
     download_result = subprocess.run(["git", "fetch", "origin"], check=True, text=True, capture_output=True)
     reset_result = subprocess.run(["git", "reset", "--hard", "origin/main"], check=True, text=True, capture_output=True)
-    if download_result.returncode == 0:
-        shutil.rmtree(pro_dir+"_bak")
+    if download_result.returncode == 0 and reset_result.returncode == 0:
         print("Am_Store updated success!")
     else:
-        os.rename(pro_dir+"_bak", pro_dir)
         print("Am_Store updated failed!")
