459d579f0c2f2ff87e5f3004da4b856a067fbdbf
Vaccummer <2316374406@qq.com>
2024-07-07 15:57:02 +0800

    https://github.com/Vaccummer/am_store.git

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 # date: 2024-07-4
 # coding: utf-8
 # version: 1.0
-# address: "https://github.com/Vaccummer/am_store.git"
+# address: https://github.com/Vaccummer/am_store.git
 
 import os
 wd = os.path.dirname(os.path.abspath(__file__))
